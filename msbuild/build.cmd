e7f7abba5296968437a0b5f01985acf193b764d8
mcgilberth <none@none.com>
2011-02-11 01:47:40 +0000

    added missing file, correct build.cmd in msbuild port

--- msbuild/build.cmd
+++ msbuild/build.cmd
@@ -2,18 +2,13 @@
 setlocal
 set BUILD_TARGET=%~1
 set BUILD_CONFIGURATION=%~2
-:: you can specify your own svn binary directory by setting the BUILD_SVNDIR environment variable
-if NOT EXIST "%BUILD_SVNDIR%" (set BUILD_SVNDIR=%ProgramFiles%\Subversion)
-if "%BUILD_TARGET%"=="" set BUILD_TARGET=all
-if "%BUILD_CONFIGURATION%"=="" set BUILD_CONFIGURATION=Debug
+if "%BUILD_TARGET%"=="" set BUILD_TARGET=build
+if "%BUILD_CONFIGURATION%"=="" set BUILD_CONFIGURATION=Release
 if "%BUILD_CONFIGURATION%"=="debug" set BUILD_CONFIGURATION=Debug
 if "%BUILD_CONFIGURATION%"=="release" set BUILD_CONFIGURATION=Release
 if "%FrameworkVersion%"=="" set FrameworkVersion=v3.5
 if "%FrameworkDir%"=="" set FrameworkDir=%SystemRoot%\Microsoft.NET\Framework
 echo Framework: %FrameworkDir%
-PATH=%PATH%;%BUILD_SVNDIR%;%FrameworkDir%\%FrameworkVersion%;%ProgramFiles%\Microsoft Visual Studio 8\VC\BIN;%ProgramFiles%\Microsoft Visual Studio 8\Common7\Tools;%ProgramFiles%\NUnit 2.4.1\bin;%ProgramFiles%\doxygen\bin
-echo %PATH%
-set PYTHON_INSTALL=C:\Python25
-echo Using python from %PYTHON_INSTALL%
-msbuild.exe LicensesCollector.proj /t:%BUILD_TARGET% /p:Configuration=%BUILD_CONFIGURATION%
+echo Framework version: %FrameworkVersion%
+%FrameworkDir%\%FrameworkVersion%\msbuild.exe LicensesCollector.sln /t:%BUILD_TARGET% /p:Configuration=%BUILD_CONFIGURATION%
 endlocal
---
+++ msbuild/src/AppSecInc.LicensesCollector/CombineLicenses.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace AppSecInc.LicensesCollector
+{
+  /// <summary>
+  /// Custom MSBuild task to combine
+  /// third party licenses.
+  /// </summary>
+  public class CombineLicenses : Task
+  {
+    #region fields
+    private String _xslFile;
+    private String _toDir;
+    private ITaskItem[] _manifests;
+
+    #endregion
+
+    public override bool Execute()
+    {
+      try
+      {
+        if (_toDir == null)
+        {
+          throw new Exception("license-manifests: missing 'toDir'");
+        }
+
+        if (_xslFile == null)
+        {
+          //Look in the same folder as the dll file.
+          Uri codeBasePath = new Uri(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase), UriKind.Absolute);
+          String path = codeBasePath.AbsolutePath;
+          String xslFilePath = path + @"\manifest.xsl";
+          FileInfo xslFile = new FileInfo(xslFilePath);
+          _xslFile = xslFile.FullName;
+        }
+
+        Log.LogMessage(MessageImportance.Normal, "license-manifests: collecting manifests to " + _toDir, null);
+
+        Directory.CreateDirectory(_toDir);
+
+        LicenseFilesManifest combinedManifest = new LicenseFilesManifest();
+        foreach (ITaskItem item in _manifests)
+        {
+          int count = 0;
+          LicenseFilesManifest manifest = new LicenseFilesManifest();
+          manifest.SetSrcDir(item.GetMetadata("Fullpath"));
+          foreach (LicenseInfo licenseInfo in manifest.GetLicenses())
+          {
+            combinedManifest.Add(licenseInfo);
+            count++;
+          }
+          foreach (FileInfo licenseFile in manifest.SrcDir.GetFiles())
+          {
+            if (!licenseFile.Name.Equals("manifest.xml"))
+            {
+              String targetLicenseFile = Path.Combine(_toDir, licenseFile.Name);
+              File.Copy(licenseFile.FullName, targetLicenseFile, true);
+            }
+          }
+          Log.LogMessage(MessageImportance.Normal, "merged " + count + " license(s) from " + manifest.SrcDir.FullName);
+        }
+
+        String manifestXsl = null;
+        if (_xslFile != null)
+        {
+          manifestXsl = Path.Combine(_toDir, "manifest.xsl");
+          Log.LogMessage(MessageImportance.Normal, "copying " + _xslFile + " to " + _toDir);
+          File.Copy(_xslFile, manifestXsl, true);
+        }
+
+        FileInfo manifestFile = new FileInfo(Path.Combine(_toDir, "manifest.xml"));
+        Log.LogMessage(MessageImportance.Normal, "writing " + manifestFile);
+        combinedManifest.WriteTo(manifestFile, manifestXsl != null ? manifestXsl : null);
+
+        return true;
+      }
+      catch (Exception e)
+      {
+        Log.LogMessage(MessageImportance.High, "error: " + e.Message);
+        Console.WriteLine(e.StackTrace);
+        throw;
+      }
+    }
+
+    #region properties
+    [Required]
+    public String ToDir
+    {
+      get { return _toDir; }
+      set { _toDir = value; }
+    }
+
+    public String XslFile
+    {
+      get { return _xslFile; }
+      set { _xslFile = value; }
+    }
+
+    public ITaskItem[] Manifests
+    {
+      get { return _manifests; }
+      set { _manifests = value; }
+    }
+
+    #endregion
+
+  }
+}
